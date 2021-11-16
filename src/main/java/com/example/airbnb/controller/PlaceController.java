a275149b4cacf58fe11bf4c2b922353db78e16f1
AndreArjDev <andre.021arj@gmail.com>
2021-11-16 17:13:58 -0300

    segunda config cors

--- src/main/java/com/example/airbnb/controller/PlaceController.java
+++ src/main/java/com/example/airbnb/controller/PlaceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,19 +18,20 @@ import com.example.airbnb.repository.PlaceRepository;
 
 import com.example.airbnb.model.Place;
 @RestController
+@CrossOrigin
 @RequestMapping("/place")
 public class PlaceController {
 	
 	@Autowired
 	private PlaceRepository placeRepository;
 	
-	
+	@CrossOrigin
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
 	public List<Place> findAll(){
 		return placeRepository.findAll();
 	}
 	
-	
+	@CrossOrigin
 	@RequestMapping(value = "{city}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<List<Place>> findByLocalidade(@PathVariable("city") String city) {
 		List<Place> places = this.placeRepository.findByCity(city);
@@ -39,7 +41,7 @@ public class PlaceController {
 		return new ResponseEntity<List<Place>>(places, HttpStatus.OK);
 		
 	}
-	
+	@CrossOrigin
 	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
 	public ResponseEntity<Place> addPlace(@RequestBody Place place){
 		return new ResponseEntity<Place>(placeRepository.save(place),HttpStatus.CREATED);
