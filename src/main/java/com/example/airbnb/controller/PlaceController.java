5de604aeffb7ec59091b4c74469d39c57d9ecb13
AndreArjDev <andre.021arj@gmail.com>
2021-11-16 07:41:31 -0300

    method post

--- src/main/java/com/example/airbnb/controller/PlaceController.java
+++ src/main/java/com/example/airbnb/controller/PlaceController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +37,11 @@ public class PlaceController {
 			return new ResponseEntity<List<Place>>(places, HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<List<Place>>(places, HttpStatus.OK);
+		
+	}
 	
-	
+	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<Place> addPlace(@RequestBody Place place){
+		return new ResponseEntity<Place>(placeRepository.save(place),HttpStatus.CREATED);
 	}
 }
